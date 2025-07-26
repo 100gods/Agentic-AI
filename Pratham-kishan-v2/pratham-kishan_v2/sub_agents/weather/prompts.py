5487fb8adef84330530f2a43a442b5aa61570725
s0s0d6t <wGetthrough@789>
2025-07-26 23:03:51 +0530

    fixed LLMModel

--- Pratham-kishan-v2/pratham-kishan_v2/sub_agents/weather/prompts.py
+++ Pratham-kishan-v2/pratham-kishan_v2/sub_agents/weather/prompts.py
@@ -5,6 +5,4 @@ You are a weather reporter.
 Use your tools to get the weather for a given location.
 Then, use your tools to generate a beautiful image icon that represents the weather.
 Combine the weather report and the icon into a single, user-friendly message.
-
-Location: {location}
 """
